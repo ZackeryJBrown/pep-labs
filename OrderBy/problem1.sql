d40781dc6daffff7a1b63d4b953ce9518ce88271
ZackeryJBrown <zackeryjbrown@gmail.com>
2023-01-10 20:31:28 +0000

    Saved progress on OrderBy 2023-01-10 20:31:28 +0000

---
+++ OrderBy/problem1.sql
@@ -0,0 +1,8 @@
+SELECT * FROM character ORDER BY last_name, first_name;
+
+
+
+ /**
+         * Problem 1: Write a statement below to query the database for all characters. Make sure the results are in
+         * ascending order by last name, and first name as a tie-breaker.
+         */
