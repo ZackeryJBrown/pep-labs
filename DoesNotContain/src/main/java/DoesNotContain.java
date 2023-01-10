f0d7b68f854daa53b36d76c11d0a01e7377f1160
ZackeryJBrown <zackeryjbrown@gmail.com>
2023-01-10 20:58:56 +0000

    Saved progress on DoesNotContain 2023-01-10 20:58:56 +0000

--- DoesNotContain/src/main/java/DoesNotContain.java
+++ DoesNotContain/src/main/java/DoesNotContain.java
@@ -13,6 +13,16 @@ public class DoesNotContain {
      * @return true if arr does not contain target. false otherwise.
      */
     public boolean arrayDoesNotContain(int[] arr, int target){
+        boolean logicSwitch = false;
+    
+        for (int i = 0 ; i < arr.length ; i++){
+            if (arr[i] == target){
+                logicSwitch = true;
+                }
+            }
+        if (!logicSwitch){
+            return true;
+        }
         return false;
     }
 }
