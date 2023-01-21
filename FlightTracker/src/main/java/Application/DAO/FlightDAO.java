3889e6857ba259bcc97a1c910e359c087afc793e
ZackeryJBrown <zackeryjbrown@gmail.com>
2023-01-21 16:12:43 +0000

    Saved progress on FlightTracker 2023-01-21 16:12:43 +0000

--- FlightTracker/src/main/java/Application/DAO/FlightDAO.java
+++ FlightTracker/src/main/java/Application/DAO/FlightDAO.java
@@ -108,7 +108,7 @@ public class FlightDAO {
         try {
             //Write SQL logic here. When inserting, you only need to define the departure_city and arrival_city
             //values (two columns total!)
-            String sql = "INSERT INTO flight (ColumnName1, ColumnName2) VALUES (?, ?);";
+            String sql = "INSERT INTO flight (departure_city, arrival_city) VALUES (?, ?);";
             PreparedStatement preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
 
             //write preparedStatement's setString and setInt methods here.
@@ -148,7 +148,7 @@ public class FlightDAO {
         Connection connection = ConnectionUtil.getConnection();
         try {
             //Write SQL logic here
-            String sql = "UPDATE flight SET (departure_city=?, arrival_city=?) WHERE flight_id=?;";
+            String sql = "UPDATE flight SET departure_city=?, arrival_city=? WHERE flight_id=?;";
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
 
             //write PreparedStatement setString and setInt methods here.
