1ff2e5cba1dfac13a49440a6b2ef2c053b70682e
ZackeryJBrown <zackeryjbrown@gmail.com>
2023-01-24 18:56:49 +0000

    Saved progress on CombineArrays 2023-01-24 18:56:49 +0000

--- CombineArrays/src/main/java/CombineArrays.java
+++ CombineArrays/src/main/java/CombineArrays.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+
+
 
 public class CombineArrays {
     /**
@@ -8,7 +11,25 @@ public class CombineArrays {
      * @return an array containing the contents of arr1 followed by the contents of arr2.
      */
     public int[] combine(int[] arr1, int[] arr2){
+        ArrayList<Integer> combinedArr = new ArrayList<Integer>();
 
-        return null;
+        for (int i = 0 ; i < arr1.length ; i++){
+            combinedArr.add(arr1[i]);
+        }
+
+        for (int j = 0 ; j < arr2.length ; j++){
+            combinedArr.add(arr2[j]);
+
+        }
+
+        //int[] returnArr = new int[arr1.length + arr2.length];
+        
+        int[] returnArr = new int[combinedArr.size()];
+
+        for (int i = 0 ; i < combinedArr.size() ; i++){
+            returnArr[i] = combinedArr.get(i);
+        }
+
+        return returnArr;
     }
 }
